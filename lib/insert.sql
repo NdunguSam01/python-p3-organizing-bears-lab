9e1bdf120d2166e65f4f561a79e2052341e9ed77
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-15 11:18:44 +0300

    Lab done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr Chocolate",12,"M","Brown","High",0),
+("Rowdy",10,"F","Black","Low",1),
+("Tabitha",6,"F","Pink","Medium",0),
+("Sergeant Brown",14,"M","Brown","Low",0),
+("Melissa",11,"M","Green","High",1),
+("Grinch",12,"M","White","High",0),
+("Wendy",8,"F","White","High",0),
+(NULL,12,"M","Brown","High",1);
